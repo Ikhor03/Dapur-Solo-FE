052fe39e5eae60e70e43bc715fca59d7af94290b
Ikhor03 <imamkhoiri725@gmail.com>
2023-06-05 16:02:34 +0700

    update caption

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,12 +6,16 @@ export default function Home() {
 
     return (
         <>
-            <Navbar  />
+            <Navbar />
             <div className="bg-gray-100">
                 <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                     <div className="border-b-2 md:flex py-16 items-center">
                         <h2 className=" basis-1/2 text-2xl lg:text-5xl mb-3 md:mb-0 tracking-tight text-gray-900">Selera Nusantara</h2>
-                        <p className=" basis-1/2 text-md lg:text-2xl tracking-tight text-gray-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel metus vitae nunc tincidunt cursus vel a turpis. Ut facilisis hendrerit eros at ultricies. Curabitur sollicitudin malesuada velit, quis luctus risus consectetur vel. </p>
+                        <div className="basis-1/2">
+                            <p className="text-md lg:text-2xl tracking-tight text-gray-900">
+                                Dengan visi melestarikan budaya Indonesia terutama makanan tradisional Jawa, kami berkomitmen untuk terus mengembangkan kualitas produk kami melalui pelayanan yang prima.
+                            </p>
+                        </div>
                     </div>
 
                     <CardList />
