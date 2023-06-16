0875b74f9bcba05688dfb809e3c72f4f50406977
Ikhor03 <imamkhoiri725@gmail.com>
2023-06-16 16:29:14 +0700

    update invoice page

--- src/pages/invoices.js
+++ src/pages/invoices.js
@@ -10,8 +10,7 @@ export default function Invoice() {
     let { id } = useParams()
     const invoiceState = useSelector((state) => state.invoice)
     const invoiceLS = localStorage.getItem('invoice') ? JSON.parse(localStorage.getItem('invoice')) : {}
-    const [dataInvoice, setDataInvoice] = useState([])
-    const thisInvoice = dataInvoice.filter(item => item._id === id)
+    const [dataInvoice, setDataInvoice] = useState(null)
     const endPoint = process.env.REACT_APP_END_POINT
     const [flag, setFlag] = useState(false)
 
@@ -58,7 +57,7 @@ export default function Invoice() {
                             <dl>
                                 <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt className="text-sm font-medium text-gray-500">Status</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{thisInvoice[0].status}</dd>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{dataInvoice.status}</dd>
                                 </div>
                                 <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt className="text-sm font-medium text-gray-500">Order ID</dt>
